e3b1cff36d7492ac2ae896fbb17b63c9e6325edc
harkespan <octone.harry@gmail.com>
2016-04-06 12:43:12 +0700

    memindahkan instance untuk koneksi database ke dalam konstruktor

--- models/Model.php
+++ models/Model.php
@@ -3,25 +3,28 @@
 include_once("libraries/Database.php");
 
 class Model {
+	function __construct()
+	{
+		$this->dbh = Database::getInstance();
+	}
 
 
 	function simpanData($nim,$nama)
 	{
-		$dbh = Database::getInstance(); //digunakan setiap mau melakukan query
-		$rs = $dbh->query("INSERT INTO mhs (nim,nama) VALUES ('".$nim."','".$nama."')");
+		$rs = $this->dbh->query("INSERT INTO mhs (nim,nama) VALUES ('".$nim."','".$nama."')");
 	}
 
 	function lihatData()
 	{
-		$dbh = Database::getInstance();
-		$rs = $dbh->query("SELECT * FROM mhs");
+
+		$rs = $this->dbh->query("SELECT * FROM mhs");
 		return $rs;
 	}
 
 	function lihatDataDetail($id)
 	{
-		$dbh = Database::getInstance();
-		$rs = $dbh->query("SELECT * FROM mhs WHERE id=".$id);
+
+		$rs = $this->dbh->query("SELECT * FROM mhs WHERE id=".$id);
 		return $rs->fetch();// kalau hasil query hanya satu, gunakan method fetch() bawaan PDO
 	}
 	
